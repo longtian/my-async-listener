4919117f156c313272c4030213e8574e55086634
wangyan <wyvernnot@gmail.com>
2015-03-10 12:10:17 +0800

    track resolving time

--- example/dns.js
+++ example/dns.js
@@ -2,7 +2,19 @@ var dns = require("dns");
 
 require("../").apply();
 
-dns.resolve("www.baidu.com", function (error, ip) {
+process.addAsyncListener({
+    create: function (context, store) {
+        var hrtime = process.hrtime();
+        store.begin = hrtime[0] * 1E9 + hrtime[1];
+    },
+    after: function (context, store) {
+        var hrtime = process.hrtime();
+        var diffNs = hrtime[0] * 1E9 + hrtime[1] - store.begin;
+        console.log("diff is %d ns!", diffNs);
+    }
+}, {})
+
+dns.resolve("www.oneapm.com", function (error, ip) {
     if (error) {
         throw new Error(error);
     }
